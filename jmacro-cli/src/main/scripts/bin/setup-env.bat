273c659fd8747fe42aea64cb19abc52e6ee3376e
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2021-12-16 14:27:21 -0300

    Set max memory on setup-setup-env.bat

--- jmacro-cli/src/main/scripts/bin/setup-env.bat
+++ jmacro-cli/src/main/scripts/bin/setup-env.bat
@@ -2,4 +2,4 @@ rem "Change codepage to 65001 - UTF-8"
 chcp 65001
 set JAVA_HOME="%BASEDIR%\java\"
 set JAVACMD="%BASEDIR%\java\bin\java.exe"
-set JAVA_OPTS=-Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Djavax.net.ssl.trustStore="NUL" -Djavax.net.ssl.trustStoreType="Windows-ROOT"
+set JAVA_OPTS=-Xms512M -Xmx2G -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Djavax.net.ssl.trustStore="NUL" -Djavax.net.ssl.trustStoreType="Windows-ROOT"
