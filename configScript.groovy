e8ca07b8293fa433b68398b7e5dec3a2e44baa96
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2022-03-16 17:56:45 -0300

    Keep specs and tests compiling dynamically

--- configScript.groovy
+++ configScript.groovy
@@ -1,22 +1,46 @@
-//import org.codehaus.groovy.control.customizers.ImportCustomizer
-import java.util.logging.Level;
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.control.SourceUnit
+
+import java.util.logging.Level
 import java.util.logging.Logger
 
 def logger = Logger.getLogger("groovy.configScript")
-def log = { String msg -> logger.log(Level.INFO, msg) }
+def log = { Object msg -> logger.log(Level.INFO, msg?.toString() ?: '<<NULL>>') }
 
-log "Configuring groovy compiler"
+boolean debug = false
+
+String rootProjectPath = this.class.protectionDomain.codeSource.location.path
+rootProjectPath = rootProjectPath.substring(0, rootProjectPath.lastIndexOf('/') + 1)
+
+log rootProjectPath
+
+def basenameValidator = { String basename ->
+    // compile static if not tests/specs files
+    boolean allowed = !(basename.endsWith('Spec') || basename.endsWith('Test'))
+    if (debug && !allowed) {
+        log "${basename}: switching to Compile Dynamic"
+    }
+    return allowed
+}
+
+def unitValidator = { SourceUnit sourceUnit ->
+    String name = sourceUnit.name.replace('\\', '/')
+    name = name.startsWith('/') ? name : "/${name}"
+    name = name.replace(rootProjectPath, '')
+    // compile static if not tests module or sources
+    boolean allowed = !name.contains('/src/test/')
+    if (debug && !allowed) {
+        log "${name}: switching to Compile Dynamic"
+    }
+    return allowed
+}
 
 withConfig(configuration) {
-    // new ImportCustomizer().addImports('org.some.*')
-    // log "AST:"
-    // log "    CompileStatic"
-    // ast groovy.transform.CompileStatic
+    log "Extension: groovy"
+    log "    Compile Static for non Specs/Tests"
 
-    source(extension: 'groovy') {
-        log "Extension: groovy"
-        log "    Compile Static by default"
-        ast groovy.transform.CompileStatic
+    source(extension: 'groovy', basenameValidator: basenameValidator, unitValidator: unitValidator) {
+        ast CompileStatic
     }
 
     // start 'org.some'
