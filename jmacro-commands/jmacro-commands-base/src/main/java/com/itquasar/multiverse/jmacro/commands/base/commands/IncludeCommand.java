cdd47c4d57b5cc96635f8d833e3e267de4412fda
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2022-07-14 01:06:47 -0300

    Fix relative inclusion on IncludeCommand

--- jmacro-commands/jmacro-commands-base/src/main/java/com/itquasar/multiverse/jmacro/commands/base/commands/IncludeCommand.java
+++ jmacro-commands/jmacro-commands-base/src/main/java/com/itquasar/multiverse/jmacro/commands/base/commands/IncludeCommand.java
@@ -1,7 +1,20 @@
 package com.itquasar.multiverse.jmacro.commands.base.commands;
 
-import com.itquasar.multiverse.jmacro.commands.base.providers.ArgsCommandProvider;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
 import com.itquasar.multiverse.jmacro.commands.base.providers.ExportCommandProvider;
+import com.itquasar.multiverse.jmacro.commands.base.providers.IncludeCommandProvider;
 import com.itquasar.multiverse.jmacro.core.Command;
 import com.itquasar.multiverse.jmacro.core.JMacroCore;
 import com.itquasar.multiverse.jmacro.core.SPILoader;
@@ -10,15 +23,8 @@ import com.itquasar.multiverse.jmacro.core.command.Doc;
 import com.itquasar.multiverse.jmacro.core.exception.JMacroException;
 import com.itquasar.multiverse.jmacro.core.repository.GlobalScriptRepository;
 import com.itquasar.multiverse.jmacro.core.script.Script;
-import groovy.lang.Closure;
-import org.apache.logging.log4j.Logger;
 
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
-import java.net.URI;
-import java.net.URL;
-import java.util.*;
+import groovy.lang.Closure;
 
 @Doc("Allows script inclusion, of any extensions installed on the engine.")
 public class IncludeCommand extends Command {
@@ -29,103 +35,99 @@ public class IncludeCommand extends Command {
     @Doc("File extension of running script.")
     private final String extension;
 
-    public IncludeCommand(String name, GlobalScriptRepository repository, JMacroCore core, ScriptEngine scriptEngine) {
+    public IncludeCommand(final String name, final GlobalScriptRepository repository, final JMacroCore core, final ScriptEngine scriptEngine) {
         super(name, core, scriptEngine);
         this.repository = repository;
         this.extension = scriptEngine.getFactory().getExtensions().get(0);
     }
 
-    @Doc(
-        """
-            Include all exported objects from included scripts.
-            If extension is not supplied, same extensions as origin script will be used.
+    @Doc("""
+         Include all exported objects from included scripts.
+         If extension is not supplied, same extensions as origin script will be used.
 
-            ```
-            include 'script_name_2.ext', 'script_name_2'
-            ```
+         ```
+         include 'script_name_2.ext', 'script_name_2'
+         ```
 
-            See `export` command.
-            """
-    )
-    public void call(@Doc(name = "includeName") String... includeName) {
-        Arrays.stream(includeName).forEach(it ->
-            this.call(includeName)
-        );
+         See `export` command.
+         """)
+    public void call(@Doc(name = "includeName") final String... includeName) {
+        Arrays.stream(includeName).forEach(it -> this.call(includeName));
     }
 
-    public void call(@Doc(name = "includeName") String includeName) {
-        new Inclusion(this, getCore(), getScriptEngine(), Collections.emptyList()).from(includeName);
+    public void call(@Doc(name = "includeName") final String includeName) {
+        new Inclusion(this, this.getCore(), this.getScriptEngine(), Collections.emptyList()).from(includeName);
     }
 
     @Doc("include { ObjA, ObjB } from 'script_name.ext'.")
-    public Inclusion call(@Doc(name = "includeObjects") Closure contextNames) {
-        ContextName contextName = new ContextName();
+    public Inclusion call(@Doc(name = "includeObjects") final Closure contextNames) {
+        final var contextName = new ContextName();
         contextNames.setDelegate(contextName);
         contextNames.setResolveStrategy(Closure.DELEGATE_FIRST);
         contextNames.call();
-        return new Inclusion(this, getCore(), getScriptEngine(), contextName.names);
+        return new Inclusion(this, this.getCore(), this.getScriptEngine(), contextName.names);
     }
 
     public record Inclusion(IncludeCommand includeCommand, JMacroCore core, ScriptEngine scriptEngine, List contextName) {
 
+        @SuppressWarnings("unchecked")
         void from(String includeName) {
-            Logger logger = includeCommand.getLogger();
+            final var logger = this.includeCommand.getLogger();
 
-            String extension = includeCommand.extension;
+            final var extension = this.includeCommand.extension;
             includeName = includeName.endsWith(extension) ? includeName : includeName + '.' + extension;
 
-            // FIXME use path of script calling include to allow relative imports
+            final var parentScript = (Script) ((Map<String, Object>) this.scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE)).get("__SCRIPT__");
+            final var includePath = Paths.get(parentScript.getPath()).resolveSibling(includeName).toString().replace("\\", "/");
+
             // FIXME create syntax for abosulte and relaive? (like @ prefix on node?)
 
             // FIXME allow repository selection on inclusion
 
-            Optional<Script> scriptOptional = includeCommand.repository.get(includeName);
+            final var scriptOptional = this.includeCommand.repository.get(includePath.toString());
             if (scriptOptional.isPresent()) {
-                Script script = scriptOptional.get();
+                final var script = scriptOptional.get();
 
                 logger.debug("Including " + includeName);
 
                 try {
-                    core.getEngine().executeInclusion(
-                        script,
-                        (ScriptEngine engine) -> {
-                            includeCommand.getScriptEngine().getBindings(ScriptContext.ENGINE_SCOPE).forEach(
-                                (key, value) -> {
-                                    if (!key.startsWith("__") && !key.equals("export") && !key.equals(ARGS)) {
-                                        logger.debug("Transferring [" + key + "] to new engine");
-                                        engine.getBindings(ScriptContext.ENGINE_SCOPE).put(key, value);
-                                    }
-                                    // FIXME APPEND PARENT SCRIPT ARGS
-                                    // if (key.equals(ARGS)) {
-                                    //     ArgsCommandProvider.ArgsCommand args = (ArgsCommandProvider.ArgsCommand) engine.getBindings(ScriptContext.ENGINE_SCOPE).get(ARGS);
-                                    //     args.appendArgs()
-                                    // }
-                                });
-                            SPILoader.load(CommandProvider.class).forEachRemaining(provider -> {
-                                if (ExportCommandProvider.class.isInstance(provider)) {
-                                    logger.debug("Creating [" + provider.getName() + "] command in new engine");
-                                    provider.getCommand(this.core, engine);
-                                }
-                            });
-                        },
-                        (ScriptEngine engine) -> {
-                            Map exportsMap = (Map) engine.getBindings(ScriptContext.GLOBAL_SCOPE).get(ExportCommand.EXPORTS_KEY);
-                            Set<String> contextNames = new HashSet<>(contextName);
-                            if (contextNames.isEmpty()) {
-                                contextNames = exportsMap.keySet();
+                    this.core.getEngine().executeInclusion(script, (final var engine) -> {
+                        this.includeCommand.getScriptEngine().getBindings(ScriptContext.ENGINE_SCOPE).forEach((key, value) -> {
+                            if (!key.startsWith("__") && !key.equals("export") && !key.equals("include")&& !key.equals(ARGS)) {
+                                logger.debug("Transferring [" + key + "] to new engine");
+                                engine.getBindings(ScriptContext.ENGINE_SCOPE).put(key, value);
                             }
-                            if(contextNames.isEmpty()) {
-                                logger.warn("Nothing will be included in original engine");
-                            } else {
-                                logger.warn(contextNames + " will be included in original engine");
-                            }
-                            contextNames.forEach(name -> {
-                                Object library = exportsMap.get(name);
-                                logger.debug("Including [" + name + "] to original engine (" + library + ")");
-                                includeCommand.getScriptEngine().getBindings(ScriptContext.ENGINE_SCOPE).put(name, library);
-                            });
+                            // FIXME APPEND PARENT SCRIPT ARGS
+                            // if (key.equals(ARGS)) {
+                            // ArgsCommandProvider.ArgsCommand args = (ArgsCommandProvider.ArgsCommand)
+                            // engine.getBindings(ScriptContext.ENGINE_SCOPE).get(ARGS);
+                            // args.appendArgs()
+                            // }
                         });
-                } catch (ScriptException e) {
+                        SPILoader.load(CommandProvider.class).forEachRemaining(provider -> {
+                            if ((provider instanceof ExportCommandProvider) || (provider instanceof IncludeCommandProvider)) {
+                                logger.debug("Creating [" + provider.getName() + "] command in new engine");
+                                provider.getCommand(this.core, engine);
+                            }
+                        });
+                    }, (final var engine) -> {
+                        final var exportsMap = (Map) engine.getBindings(ScriptContext.GLOBAL_SCOPE).get(ExportCommand.EXPORTS_KEY);
+                        Set<String> contextNames = new HashSet<>(this.contextName);
+                        if (contextNames.isEmpty()) {
+                            contextNames = exportsMap.keySet();
+                        }
+                        if (contextNames.isEmpty()) {
+                            logger.warn("Nothing will be included in original engine");
+                        } else {
+                            logger.warn(contextNames + " will be included in " + parentScript.getPath() + " engine");
+                        }
+                        contextNames.forEach(name -> {
+                            final var library = exportsMap.get(name);
+                            logger.debug("Including [" + name + "] to original engine (" + library + ")");
+                            this.includeCommand.getScriptEngine().getBindings(ScriptContext.ENGINE_SCOPE).put(name, library);
+                        });
+                    });
+                } catch (final ScriptException e) {
                     throw new JMacroException("Error including script " + includeName, e);
                 }
             } else {
@@ -138,8 +140,8 @@ public class IncludeCommand extends Command {
 
         private final List<String> names = new ArrayList<>();
 
-        public void propertyMissing(String name) {
-            names.add(name);
+        public void propertyMissing(final String name) {
+            this.names.add(name);
         }
     }
 }
