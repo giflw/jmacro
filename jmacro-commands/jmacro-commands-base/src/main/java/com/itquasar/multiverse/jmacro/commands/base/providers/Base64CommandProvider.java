755c3df9e09c9b4aced91860dc86867041f1eb6d
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2022-07-20 22:41:20 -0300

    Update base64 command to decode InputStreams

--- jmacro-commands/jmacro-commands-base/src/main/java/com/itquasar/multiverse/jmacro/commands/base/providers/Base64CommandProvider.java
+++ jmacro-commands/jmacro-commands-base/src/main/java/com/itquasar/multiverse/jmacro/commands/base/providers/Base64CommandProvider.java
@@ -1,12 +1,21 @@
 package com.itquasar.multiverse.jmacro.commands.base.providers;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+import java.util.List;
+
+import javax.script.ScriptEngine;
+
 import com.itquasar.multiverse.jmacro.core.Command;
 import com.itquasar.multiverse.jmacro.core.JMacroCore;
 import com.itquasar.multiverse.jmacro.core.command.CommandProvider;
 
-import javax.script.ScriptEngine;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+import lombok.Getter;
+import lombok.SneakyThrows;
 
 public class Base64CommandProvider implements CommandProvider<Base64CommandProvider.Base64Command> {
 
@@ -26,15 +35,35 @@ public class Base64CommandProvider implements CommandProvider<Base64CommandProvi
     }
 
     @Override
-    public Base64Command getCommand(JMacroCore jMacroCore, ScriptEngine scriptEngine) {
-        return new Base64Command(getName(), jMacroCore, scriptEngine);
+    public Base64Command getCommand(final JMacroCore jMacroCore, final ScriptEngine scriptEngine) {
+        return new Base64Command(this.getName(), jMacroCore, scriptEngine);
     }
 
     static public class Base64Command extends Command {
 
-        public Base64Command(String name, JMacroCore core, ScriptEngine scriptEngine) {
+        public enum Codec {
+
+            BASIC(Base64.getEncoder(), Base64.getDecoder()),
+            MIME(Base64.getMimeEncoder(), Base64.getMimeDecoder()),
+            URL(Base64.getUrlEncoder(), Base64.getUrlDecoder());
+
+            @Getter
+            private final Encoder encoder;
+
+            @Getter
+            private final Decoder decoder;
+
+            Codec(final Encoder encoder, final Decoder decoder) {
+                this.encoder = encoder;
+                this.decoder = decoder;
+            }
+        }
+
+        public Base64Command(final String name, final JMacroCore core, final ScriptEngine scriptEngine) {
             super(name, core, scriptEngine);
         }
+
+
 
         /**
          * Encode to base64 and return String
@@ -43,7 +72,7 @@ public class Base64CommandProvider implements CommandProvider<Base64CommandProvi
          * @param text
          * @return
          */
-        public String leftShift(String text) {
+        public String leftShift(final String text) {
             return this.encode(text);
         }
 
@@ -54,40 +83,50 @@ public class Base64CommandProvider implements CommandProvider<Base64CommandProvi
          * @param text
          * @return
          */
-        public String rightShift(String text) {
+        public String rightShift(final String text) {
             return new String(this.decode(text), StandardCharsets.UTF_8);
         }
 
-        public String call(String text) {
+        public String call(final String text) {
             return this.encode(text);
         }
 
-        public String call(byte[] bytes) {
+        public String call(final byte[] bytes) {
             return this.encode(bytes);
         }
 
-        public String enc(String text) {
+        public String enc(final String text) {
             return this.encode(text);
         }
 
-        public String encode(String text) {
+        public String encode(final String text) {
             return this.encode(text.getBytes(StandardCharsets.UTF_8));
         }
 
-        public String encode(byte[] bytes) {
-            return java.util.Base64.getEncoder().encodeToString(bytes);
+        public String encode(final byte[] bytes) {
+            return Codec.BASIC.getEncoder().encodeToString(bytes);
         }
 
-        public String dec(String text) {
+        public String dec(final String text) {
             return new String(this.decode(text), StandardCharsets.UTF_8);
         }
 
-        public byte[] decode(String text) {
+        public byte[] decode(final String text) {
             return this.decode(text.getBytes(StandardCharsets.UTF_8));
         }
 
-        public byte[] decode(byte[] bytes) {
-            return java.util.Base64.getDecoder().decode(bytes);
+        public byte[] decode(final byte[] bytes) {
+            return Codec.BASIC.getDecoder().decode(bytes);
+        }
+
+        public InputStream decode(final InputStream input) {
+            return this.decode(input, Codec.BASIC);
+        }
+
+        @SneakyThrows
+        public InputStream decode(final InputStream input, final Codec codec) {
+            final var buffer = codec.getDecoder().decode(input.readAllBytes());
+            return new ByteArrayInputStream(buffer);
         }
 
     }
