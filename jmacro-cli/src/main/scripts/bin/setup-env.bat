e67ab338a664805f659588ceaf7c2837b958d45f
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-04-18 10:53:54 -0300

    Fix jmacro-dist, knx files and setup-env

--- jmacro-cli/src/main/scripts/bin/setup-env.bat
+++ jmacro-cli/src/main/scripts/bin/setup-env.bat
@@ -1,5 +1,5 @@
 rem "Change codepage to 65001 - UTF-8"
 chcp 65001
-set JAVA_HOME="%BASEDIR%\java\"
-set JAVACMD="%BASEDIR%\java\bin\java.exe"
+set JAVA_HOME="%BASEDIR%\jdk\"
+set JAVACMD="%BASEDIR%\jdk\bin\java.exe"
 set JAVA_OPTS=-Xms512M -Xmx2G -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Djavax.net.ssl.trustStore="NUL" -Djavax.net.ssl.trustStoreType="Windows-ROOT"
