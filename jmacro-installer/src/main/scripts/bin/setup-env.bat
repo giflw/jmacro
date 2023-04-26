799f573abf9aad8b48493126812937ea5cdf639b
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-04-26 17:15:34 -0300

    Update knx launcher, fix knx/installer files

--- jmacro-installer/src/main/scripts/bin/setup-env.bat
+++ jmacro-installer/src/main/scripts/bin/setup-env.bat
@@ -3,4 +3,4 @@ chcp 65001
 set JAVA_HOME="%BASEDIR%\jre\"
 set JAVACMD="%BASEDIR%\jre\bin\java.exe"
 rem set JAVA_OPTS=-Xms512M -Xmx2G -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Djavax.net.ssl.trustStore="NUL" -Djavax.net.ssl.trustStoreType="Windows-ROOT"
-set JAVA_OPTS=-Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Djavax.net.ssl.trustStore="NUL" -Djavax.net.ssl.trustStoreType="Windows-ROOT"
+set JAVA_OPTS=-Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Djavax.net.ssl.trustStore="NUL" -Djavax.net.ssl.trustStoreType="Windows-ROOT" -Djava.net.useSystemProxies="true"
