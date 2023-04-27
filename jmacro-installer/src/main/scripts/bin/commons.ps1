0fbb27808b980c02886eed2f739a9c2ff4e52f91
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-04-27 16:29:49 -0300

    Fix installer e launcher

--- jmacro-installer/src/main/scripts/bin/commons.ps1
+++ jmacro-installer/src/main/scripts/bin/commons.ps1
@@ -2,6 +2,7 @@ $tmpName = "jmacro-$([guid]::NewGuid() )"
 $tmpPath = "${env:TEMP}/${tmpName}".replace('\', '/')
 Write-Host "EXEC TMP PATH: $(New-Item -Name "${tmpName}" -Path "${env:TEMP}" -Type Directory)"
 
+$jmacroPath = Split-Path -Path $PSScriptRoot -Parent
 $jmacroPath = "$((Split-Path -Path $PSScriptRoot -Parent).Replace('\', '/'))"
 
 $jmacroPath = "${jmacroPath}".Replace('\', '/')
