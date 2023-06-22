34b48d7a8b15778b4b9911e9831b161154c9928d
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-06-22 09:30:22 -0300

    feat(utils.net): add proxy utility functions

--- jmacro-cli.sh
+++ jmacro-cli.sh
@@ -6,19 +6,23 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
+#export JAVA_OPTS='-Xms512M -Xmx2G -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Djavax.net.ssl.trustStore="NUL" -Djavax.net.ssl.trustStoreType="Windows-ROOT" -Djava.net.useSystemProxies="true"'
+
+PROJECT=:jmacro
 CLEAN=""
 BUILD=true
 for arg in $@; do
     case "$arg" in
         "--clean" ) CLEAN=clean;;
-        "--skip-build") BUILD=false ;;
-        *) CONFIG_FILE=${arg:=$CONFIG_FILE};;
+        "--skip-build" ) BUILD=false ;;
+        :* ) PROJECT="${arg}" ;;
+        * ) CONFIG_FILE=${arg:=$CONFIG_FILE};;
     esac
-    shift
 done
 
 if [ "$BUILD" == "true" ]; then
-    mvn $CLEAN install -amd -pl :jmacro-commands-browser -DskipTests=true -P !application,!data,!installer,!jdk-embedded,!jre-embedded,!launcher,!scripts,!tools
+    echo "Building ${PROJECT}"
+    mvn $CLEAN install -amd -pl "${PROJECT}" -DskipTests=true -P "!application,!data,!installer,!jdk-embedded,!jre-embedded,!launcher,!scripts,!tools"
 else
     echo '!!!! Skipping build !!!!'
 fi
--- jmacro-core/src/main/java/com/itquasar/multiverse/jmacro/core/NetUtils.java
+++ jmacro-core/src/main/java/com/itquasar/multiverse/jmacro/core/NetUtils.java
@@ -4,7 +4,8 @@ import lombok.experimental.UtilityClass;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.*;
+import java.util.Optional;
 
 @Log4j2
 @UtilityClass
@@ -26,4 +27,34 @@ public class NetUtils {
             return 0;
         }
     }
+
+    public static Optional<Proxy> proxyFor(URI uri) {
+        LOGGER.warn("Searching proxy configuration using " + uri);
+        return ProxySelector.getDefault().select(uri).stream()
+            .peek(it -> LOGGER.info("Proxy (existing): " + it))
+            .filter(it -> it.type() == java.net.Proxy.Type.HTTP)
+            .peek(it -> LOGGER.debug("Proxy (found for uri): " + it + " (" + uri + ")"))
+            .findFirst();
+    }
+
+    public static String proxyAddressFor(URI uri) {
+        // FIXME use configuration on jmacro.yaml to set this to false if needed
+        String useSystemProxiesKey = "java.net.useSystemProxies";
+        if (System.getProperty(useSystemProxiesKey) == null) {
+            System.setProperty(useSystemProxiesKey, "true");
+        }
+
+        String proxyAddress = null;
+        Optional<Proxy> proxy = proxyFor(uri);
+        if (proxy.isPresent() && proxy.get().address() instanceof InetSocketAddress address) {
+            proxyAddress = address.getHostName() + ":" + address.getPort();
+            LOGGER.warn("Using proxy address " + proxyAddress + " for uri " + uri);
+        }
+        return proxyAddress;
+    }
+
+    public static String proxyAddressFor(String uri) {
+        return proxyAddressFor(URI.create(uri));
+    }
+
 }
