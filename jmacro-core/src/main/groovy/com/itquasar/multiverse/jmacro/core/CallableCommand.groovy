10a40f5b96076eb36be1290108aba8d6fdebefd4
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-07-12 00:50:50 -0300

    feat(core): refactor CallableCommand to use consumer instead of Closure

--- jmacro-core/src/main/groovy/com/itquasar/multiverse/jmacro/core/CallableCommand.groovy
+++ jmacro-core/src/main/groovy/com/itquasar/multiverse/jmacro/core/CallableCommand.groovy
@@ -1,18 +1,39 @@
 package com.itquasar.multiverse.jmacro.core
 
+import groovy.transform.CompileDynamic
 
 import javax.script.ScriptEngine
+import java.lang.reflect.Proxy
+import java.util.function.Consumer
 
-abstract class CallableCommand extends Command {
+abstract class CallableCommand<T> extends Command {
+
+    private static final String[] CALL_ALTERNATIVES = ['apply', 'invoke']
 
     CallableCommand(String name, Core core, ScriptEngine scriptEngine) {
         super(name, core, scriptEngine)
     }
 
-    def call(Closure closure) {
-        closure.delegate = this
-        closure.resolveStrategy = Closure.DELEGATE_FIRST
-        closure()
-        this
+    @CompileDynamic
+    T call(Consumer<T> consumer) {
+        consumer.accept((T) this)
+        return (T) this
+    }
+
+    T invoke(Consumer<T> consumer) {
+        this.call(consumer)
+    }
+
+    T apply(Consumer<T> consumer) {
+        this.call(consumer)
+    }
+
+    @Override
+    @CompileDynamic
+    def methodMissing(String name, def args) {
+        if (name in CALL_ALTERNATIVES) {
+            return this.call(*args)
+        }
+        return this.bindings."$name"(*args)
     }
 }
