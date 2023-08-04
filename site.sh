996a29b29ad9209f6a158bee3ae6810c2282ed07
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-08-03 23:33:42 -0300

    feat(site): fix layout, add logo

--- site.sh
+++ site.sh
@@ -5,4 +5,4 @@ if [ "$1" == "--clean" ]; then
     CLEAN=clean
 fi
 echo CLEAN=$CLEAN
-mvn -P '!launcher,!installer,!jdk-embedded,!jre-embedded,!application,!data,!scripts,!tools,!extra-jlink-modules' -pl '!:tn3270j' $CLEAN site site:stage 
+mvn -P '!launcher,!installer,!jdk-embedded,!jre-embedded,!application,!data,!scripts,!tools,!extra-jlink-modules' -pl '!:tn3270j' $CLEAN verify site site:stage
